65b4fef162a2ba528f1efb59900d72bf6ec2d2b6
SerginhuXavier <paulinhu_serginhu@hotmail.com>
2018-03-30 05:42:03 -0300

    bug fix response on getcars

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -1,49 +1,52 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Car;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
-class CarController extends Controller{
-    public function createCar(Request $request){
-        
+class CarController extends Controller {
+
+    public function createCar(Request $request) {
+
         $car = Car::create($request->all());
-        
+
         return response()->json($car);
     }
-    
-    public function update($id, Request $request){
+
+    public function update($id, Request $request) {
         $car = Car::findOrFail($id);
         $car->update($request->all());
 
         return response()->json($car, 200);
     }
-    
-    public function deleteCar($id){
+
+    public function deleteCar($id) {
         $car = Car::find($id);
         $car->delete();
-        
+
         return response()->json('Removido com Sucesso');
     }
-    public function Gc($parammeters, Request $request){
+
+    public function Gc($parammeters) {
         switch ($parammeters) {
             case 'GetOneCar':
-                $id = Request::get('id');
-            return response()->json(Car::find($id));
-        break;
-    
-        case 'Make':
+                $id = Input::get('id');
+                return response()->json(Car::find($id));
+                break;
+
+            case 'Make':
+                $make = Input::get('make');
                 $carm = Car::where('make', $make);
-            return responde()->json($carm);
-        
-        case 'index':
+                return response()->json($carm);
+
+            case 'index':
                 $cars = Car::all();
-            return response()->json($cars);
-        break;
+                return response()->json($cars);
+                break;
+        }
     }
 
-    }
-    
 }
-
